51f7cd3d79fd15c8d0a1d23021ab32172b82b8c5
Pramodinipruthvi <pramodini.tr@cognitiveclouds.com>
2022-09-08 20:14:09 +0530

    updated

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   user_name:'apikey', 
   password: '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',
-  domain: 'yourdomain.com',
+  domain: 'https://sheltered-retreat-12255.herokuapp.com/',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
